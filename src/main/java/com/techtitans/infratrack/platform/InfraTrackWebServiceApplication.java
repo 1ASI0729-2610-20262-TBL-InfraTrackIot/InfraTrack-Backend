7df6af2c5ad29c16e2eef6f1002427b65a65fc7c
Aldair Ramos <u20201f051@upc.edu.pe>
2026-05-25 09:46:00 -0500

    chore: initial commit.

---
+++ src/main/java/com/techtitans/infratrack/platform/InfraTrackWebServiceApplication.java
@@ -0,0 +1,13 @@
+package com.techtitans.infratrack.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfraTrackWebServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InfraTrackWebServiceApplication.class, args);
+    }
+
+}

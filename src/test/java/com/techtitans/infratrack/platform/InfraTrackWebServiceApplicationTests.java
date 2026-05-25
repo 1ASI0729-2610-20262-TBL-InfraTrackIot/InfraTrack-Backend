0b61eb37fedf915fb03e9dea11d53ef73f10af06
Aldair Ramos <u20201f051@upc.edu.pe>
2026-05-25 09:46:00 -0500

    chore: initial commit.

---
+++ src/test/java/com/techtitans/infratrack/platform/InfraTrackWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techtitans.infratrack.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfraTrackWebServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
